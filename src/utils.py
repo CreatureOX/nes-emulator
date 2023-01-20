499a6eef744ce99b4e7060189111e30d60b7c469
CreatureOX <creatureox0726@gmail.com>
2023-01-20 23:14:01 +0800

    profiler

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
 import wrapt
 from line_profiler import LineProfiler
+import cProfile
+import pstats
 
 lp = LineProfiler()
  
-def profiler():
+def LProfiler():
     @wrapt.decorator
     def wrapper(func, instance, args, kwargs):
         global lp
@@ -12,4 +14,19 @@ def profiler():
         lp.print_stats()
         return res
  
+    return wrapper
+
+def CProfiler(dump = False, sortby = 'tottime'):
+    def wrapper(func):
+        def profiled_func(*args, **kwargs):
+            profile = cProfile.Profile()
+            profile.enable()
+            result = func(*args, **kwargs)
+            profile.disable()
+            ps = pstats.Stats(profile).sort_stats(sortby)
+            ps.print_stats()
+            if dump:
+                ps.dump_stats(func.__name__)
+            return result
+        return profiled_func
     return wrapper
