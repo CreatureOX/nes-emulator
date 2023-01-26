d386928b08b5e9f4e0e5604aa689fe1310da4b43
CreatureOX <creatureox0726@gmail.com>
2023-01-26 14:22:37 +0800

    build tool

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
-from setuptools import setup, Extension
+from setuptools import Command, setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 import os
 
+class Clean(Command):
+    description = "Clean cythonized products"
+    user_options = [('dir=', None, 'Specify the folder to clean')]
+
+    def initialize_options(self) -> None:
+        pass
+
+    def finalize_options(self) -> None:
+        pass
+
+    def run(self) -> None:
+        walks = os.walk(r'.')
+        for dirpath, dirnames, filenames in walks:
+            for filename in filenames:
+                if filename.endswith(".pyd") \
+                    or filename.endswith(".c") \
+                    or filename.endswith(".h") \
+                    or filename.endswith(".html"):
+                    os.remove(dirpath + "\\" + filename)
+
 os.chdir("./src") 
 
 setup(
+    cmdclass={
+        "clean": Clean
+    },
     ext_modules=cythonize([
         Extension("mapper", sources=["./mapper.py"]),
         Extension("cartridge", sources=["./cartridge.py"]),
