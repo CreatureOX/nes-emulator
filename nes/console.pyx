be19305f0402a1ac7ec76692091ae20d50cb3bf7
CreatureOX <creatureox0726@gmail.com>
2024-06-22 17:52:45 +0800

    cpu debug

--- nes/console.pyx
+++ nes/console.pyx
@@ -16,6 +16,7 @@ cdef class Console:
     def __init__(self, filename: str) -> None:
         cart = Cartridge(filename)
         self.bus = CPUBus(cart)
+        self.cpu_debugger = CPUDebugger(self.bus)
         self.ppu_debugger = PPUDebugger(self.bus.ppu)
 
     cpdef void power_up(self):
@@ -66,104 +67,6 @@ cdef class Console:
             self.bus.controller[0] |= 0x02
         elif pressed[K_RIGHT]:
             self.bus.controller[0] |= 0x01
-
-    cpdef dict cpu_status_info(self):
-        return {
-            "N": True if self.bus.cpu.registers.status.status_mask["N"] > 0 else False,
-            "V": True if self.bus.cpu.registers.status.status_mask["V"] > 0 else False,
-            "U": True if self.bus.cpu.registers.status.status_mask["U"] > 0 else False,
-            "B": True if self.bus.cpu.registers.status.status_mask["B"] > 0 else False,
-            "D": True if self.bus.cpu.registers.status.status_mask["D"] > 0 else False,
-            "I": True if self.bus.cpu.registers.status.status_mask["I"] > 0 else False,
-            "Z": True if self.bus.cpu.registers.status.status_mask["Z"] > 0 else False,
-        }
-
-    cpdef dict cpu_registers_info(self):
-        return {
-            "PC": self.bus.cpu.registers.PC,
-            "A": self.bus.cpu.registers.A,
-            "X": self.bus.cpu.registers.X,
-            "Y": self.bus.cpu.registers.Y,
-            "SP": self.bus.cpu.registers.SP,
-        }
-
-    cpdef str cpu_ram(self, uint16_t start_addr, uint16_t end_addr):
-        hex_code = ""
-        for addr in range(start_addr, end_addr, 16):
-            hex_code += "${addr:04X}: {codes}\n".format(\
-                addr=addr,\
-                codes=" ".join(["{hex:02X}".format(hex=self.bus.cpu.read(_addr)) for _addr in range(addr, min(addr+16, end_addr))])\
-            )
-        return hex_code
-
-    cpdef dict cpu_code_readable(self, uint16_t start_addr, uint16_t end_addr):
-        asm = {}
-
-        addr: int = start_addr
-        while addr < end_addr:
-            opcode = self.bus.read(addr, True)
-            opaddr = addr
-            addr += 1
-            op = self.bus.cpu.lookup[opcode]
-            if op.addrmode.__name__ == "IMP":
-                value = "    "
-            if op.addrmode.__name__ == "IMM":
-                value = "#${value:02X}".format(value=self.bus.read(addr, True))
-                addr += 1
-            elif op.addrmode.__name__ == "ZP0":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X}".format(value=lo) 
-            elif op.addrmode.__name__ == "ZPX":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X},X".format(value=lo) 
-            elif op.addrmode.__name__ == "ZPY":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X},Y".format(value=lo)
-            elif op.addrmode.__name__ == "IZX":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                value = "(${value:02X},X)".format(value=lo)
-            elif op.addrmode.__name__ == "IZY":
-                lo = self.bus.read(addr, True)
-                addr += 1  
-                value = "(${value:02X},Y)".format(value=lo)  
-            elif op.addrmode.__name__ == "ABS":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                hi = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X}".format(value=hi<<8|lo)
-            elif op.addrmode.__name__ == "ABX":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                hi = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X},X".format(value=hi<<8|lo)
-            elif op.addrmode.__name__ == "ABY":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                hi = self.bus.read(addr, True)
-                addr += 1
-                value = "${value:02X},Y".format(value=hi<<8|lo)
-            elif op.addrmode.__name__ == "IND":
-                lo = self.bus.read(addr, True)
-                addr += 1
-                hi = self.bus.read(addr, True)
-                addr += 1
-                value = "(${value:02X})".format(value=hi<<8|lo)
-            elif op.addrmode.__name__ == "REL":
-                inst = self.bus.read(addr, True)
-                addr += 1
-                offset = addr + <int8_t>inst
-                value = "${value:02X} [${offset:04X}]".format(value=inst,offset=offset)
-            asm[opaddr] = "${addr:04X}: {name} {value:11s} ({addrmode})".format(addr=opaddr,name=op.name,value=value,addrmode=op.addrmode.__name__)
-        return asm 
-
-    cpdef uint16_t cpu_pc(self):
-        return self.bus.cpu.registers.PC   
 
     cpdef uint8_t[:,:,:] ppu_pattern_table(self, uint8_t i):
         return self.ppu_debugger.pattern_table(i,0)
---
+++ nes/cpu_debug.pyx
@@ -0,0 +1,104 @@
+cdef class CPUDebugger:
+    def __init__(self, bus: CPUBus) -> None:
+        self.bus = bus
+
+    cpdef dict status(self):
+        return {
+            "N": self.bus.cpu.registers.status.status_mask["N"] > 0,
+            "V": self.bus.cpu.registers.status.status_mask["V"] > 0,
+            "U": self.bus.cpu.registers.status.status_mask["U"] > 0,
+            "B": self.bus.cpu.registers.status.status_mask["B"] > 0,
+            "D": self.bus.cpu.registers.status.status_mask["D"] > 0,
+            "I": self.bus.cpu.registers.status.status_mask["I"] > 0,
+            "Z": self.bus.cpu.registers.status.status_mask["Z"] > 0,
+        }
+
+    cpdef dict registers(self):
+        return {
+            "PC":  self.bus.cpu.registers.PC,
+             "A":  self.bus.cpu.registers.A,
+             "X":  self.bus.cpu.registers.X,
+             "Y":  self.bus.cpu.registers.Y,
+            "SP":  self.bus.cpu.registers.SP,
+        }
+
+    cpdef str ram(self, uint16_t start_addr, uint16_t end_addr):
+        hex_code = ""
+        for addr in range(start_addr, end_addr, 16):
+            code_group = ["{hex:02X}".format(hex = self.bus.cpu.read(_addr)) for _addr in range(addr, min(addr + 16, end_addr))]
+            hex_code += "${addr:04X}: {codes}\n".format(addr = addr, codes = " ".join(code_group))
+        return hex_code
+
+    cpdef dict to_asm(self, uint16_t start_addr, uint16_t end_addr):
+        asm = {}
+
+        addr: int = start_addr
+        while addr < end_addr:
+            opcode = self.bus.read(addr, True)
+            opaddr = addr
+            addr += 1
+            op = self.bus.cpu.lookup[opcode]
+            if op.addrmode.__name__ == "IMP":
+                value = "    "
+            if op.addrmode.__name__ == "IMM":
+                value = "#${value:02X}".format(value = self.bus.read(addr, True))
+                addr += 1
+            elif op.addrmode.__name__ == "ZP0":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X}".format(value = lo) 
+            elif op.addrmode.__name__ == "ZPX":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X},X".format(value = lo) 
+            elif op.addrmode.__name__ == "ZPY":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X},Y".format(value = lo)
+            elif op.addrmode.__name__ == "IZX":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                value = "(${value:02X},X)".format(value = lo)
+            elif op.addrmode.__name__ == "IZY":
+                lo = self.bus.read(addr, True)
+                addr += 1  
+                value = "(${value:02X},Y)".format(value = lo)  
+            elif op.addrmode.__name__ == "ABS":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                hi = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X}".format(value = hi << 8 | lo)
+            elif op.addrmode.__name__ == "ABX":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                hi = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X},X".format(value = hi << 8 | lo)
+            elif op.addrmode.__name__ == "ABY":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                hi = self.bus.read(addr, True)
+                addr += 1
+                value = "${value:02X},Y".format(value = hi << 8 | lo)
+            elif op.addrmode.__name__ == "IND":
+                lo = self.bus.read(addr, True)
+                addr += 1
+                hi = self.bus.read(addr, True)
+                addr += 1
+                value = "(${value:02X})".format(value = hi << 8 | lo)
+            elif op.addrmode.__name__ == "REL":
+                inst = self.bus.read(addr, True)
+                addr += 1
+                offset = addr + <int8_t> inst
+                value = "${value:02X} [${offset:04X}]".format(value = inst, offset = offset)
+
+            asm[opaddr] = "${addr:04X}: {name} {value:11s} ({addrmode})".format(
+                addr = opaddr,
+                name = op.name,
+                value = value,
+                addrmode = op.addrmode.__name__)
+        return asm 
+
+    cpdef uint16_t PC(self):
+        return self.bus.cpu.registers.PC
