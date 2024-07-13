eeb54fae6037b44063616a51f45a4ec0399af826
CreatureOX <creatureox0726@gmail.com>
2024-07-13 09:54:07 +0800

    setup tool

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import Command, setup
 from Cython.Build import cythonize
 import numpy as np
 import os
+import glob
+
 
 class Clean(Command):
     description = "Clean cythonized products"
@@ -27,35 +29,8 @@ setup(
     cmdclass={
         "clean": Clean
     },
-    ext_modules=cythonize([
-        "nes/mapper/mirror.pyx",
-        "nes/mapper/mapping.pyx",
-        "nes/mapper/mapper.pyx",
-        "nes/mapper/impl/mapper_nrom.pyx",
-        "nes/mapper/impl/mapper_mmc1.pyx",
-        "nes/mapper/impl/mapper_uxrom.pyx",
-        "nes/mapper/impl/mapper_mmc3.pyx",
-        "nes/mapper/impl/mapper_ines003.pyx",
-        "nes/mapper/impl/mapper_gxrom.pyx",
-        "nes/mapper/mapper_factory.pyx",
-        "nes/cart/cart.pyx",
-        "nes/cart/impl/cart_ines.pyx",
-        "nes/cart/impl/cart_nes2.pyx",
-        "nes/cart/cart_debug.pyx",
-        "nes/file_loader.pyx",
-        "nes/bus/bus.pyx",
-        "nes/cpu/registers.pyx",
-        "nes/cpu/cpu_op.pyx",
-        "nes/cpu/cpu.pyx",
-        "nes/cpu/cpu_debug.pyx",
-        "nes/ppu/ppu.pyx",
-        "nes/ppu/registers.pyx",
-        "nes/ppu/ppu_sprite.pyx",
-        "nes/ppu/ppu_debug.pyx",
-        "nes/apu/registers.pyx",
-        "nes/apu/apu.pyx",
-        "nes/console.pyx",
-    ], compiler_directives={'language_level' : "3"}, 
-    annotate=True),
+    ext_modules=cythonize(glob.glob('nes/**/*.pyx', recursive=True), 
+                          compiler_directives={'language_level' : "3"}, 
+                          annotate=True),
     include_dirs=[np.get_include()]
 )
