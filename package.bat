f703b704ae8686f3498de569089ec19d406ff931
CreatureOX <creatureox0726@gmail.com>
2025-05-31 23:23:08 +0800

    [feat] github actions: build

--- package.bat
+++ package.bat
@@ -1,1 +1,11 @@
-nuitka --windows-console-mode=disable .\nes-emulator.py
+nuitka ^
+    --report=nuitka-report.xml ^
+    --standalone ^
+    --windows-console-mode=disable ^
+    --output-dir=dist ^
+    --include-package=nes ^
+    --include-package=pygame ^
+    --enable-plugin=tk-inter ^
+    --show-progress ^
+    --verbose ^
+    nes-emulator.py
