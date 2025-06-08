b03ef741abc27565e0675f55e672a3e29ce6eedd
CreatureOX <creatureox0726@gmail.com>
2025-06-08 11:33:55 +0800

    pygame 2.x

--- gui/freesimplegui/emulator_view.py
+++ gui/freesimplegui/emulator_view.py
@@ -16,6 +16,7 @@ import time
 import sys
 import json
 import ctypes
+import platform
 
 from pathlib import Path
 
@@ -41,10 +42,15 @@ class EmulatorWindow(BaseView):
         ['Help', ['About',]],
     ]
 
-    __SIZE = ( 256 + 20, 240 + 20 )
+    # NES resolution
+    NES_WIDTH = 256
+    NES_HEIGHT = 240
+    ASPECT_RATIO = NES_WIDTH / NES_HEIGHT
+    
+    # NES Emulator window size
+    __SIZE = (NES_WIDTH + 20, NES_HEIGHT + 20)
 
     __RESIZABLE = True
-
     __FINALIZE = True
      
     def __init__(self):
@@ -54,6 +60,10 @@ class EmulatorWindow(BaseView):
                          finalize = self.__FINALIZE)
         self.__lock = Lock()
         self.__stop = Event()
+        self.__system = platform.system()
+        self.__height_scale = 1.0
+        self.__graph_width = self.NES_WIDTH
+        self.__graph_height = self.NES_HEIGHT
 
         # File Tab
         self._events["Open"] = self.__run
@@ -74,13 +84,16 @@ class EmulatorWindow(BaseView):
         # Help Tab
         self._events["About"] = self.__show_about
 
+        self._last_gui_focus_time = 0
+        self._focus_protect_seconds = 1.0
+
     def _layout(self):
         screen_layout = [
             [
                 sg.Graph(key = "-SCREEN-", 
-                         canvas_size = (256, 240), 
+                         canvas_size = (self.NES_WIDTH, self.NES_HEIGHT), 
                          graph_bottom_left = (0, 0), 
-                         graph_top_right = (256, 240), 
+                         graph_top_right = (self.NES_WIDTH, self.NES_HEIGHT), 
                          background_color = 'BLACK', 
                          expand_x = True, 
                          expand_y = True)
@@ -95,23 +108,44 @@ class EmulatorWindow(BaseView):
         sg.popup("Please select a nes file!")
 
     def __switch_to_english_input(self):
+        if self.__system != 'Windows':
+            return
         user32 = ctypes.WinDLL('user32', use_last_error = True)
         user32.LoadKeyboardLayoutW("00000409", 1)
 
     def _after_open(self) -> None:
-        # bind Graph (key = "SCREEN") with pygame window
-        os.environ['SDL_VIDEODRIVER'] = 'windib'
-        os.environ['SDL_WINDOWID'] = str(self._window['-SCREEN-'].TKCanvas.winfo_id())
-
-        # init pygame settings
-        self.__game_screen = pygame.display.set_mode((256, 240), pygame.RESIZABLE)
-        # self.__game_clock = pygame.time.Clock()
-        # self.__fps = 60
-
-        pygame.display.init()
-
-        self.__switch_to_english_input()
+        # bind Graph (key = "SCREEN") with pygame window, auto resized
+        self._window['-SCREEN-'].bind('<Configure>', 'Configure')
+        self._events["-SCREEN-Configure"] = self.__on_graph_resize
         
+        if self.__system == 'Windows':
+            os.environ['SDL_VIDEODRIVER'] = 'windows'
+            os.environ['SDL_WINDOWID'] = str(self._window['-SCREEN-'].TKCanvas.winfo_id())
+        elif self.__system == 'Darwin':
+            os.environ['SDL_VIDEODRIVER'] = 'cocoa'
+
+        # set pygame environment variables
+        pygame.display.init()
+        self.__game_screen = pygame.display.set_mode((self.NES_WIDTH, self.NES_HEIGHT), pygame.RESIZABLE)
+        pygame.display.set_caption('NES Emulator')
+
+        # self.__switch_to_english_input()
+        
+    def __on_graph_resize(self, event):
+        graph_height = self._window['-SCREEN-'].TKCanvas.winfo_height()
+        self.__height_scale = graph_height / self.NES_HEIGHT
+        scaled_width = int(self.NES_WIDTH * self.__height_scale)
+
+        self.__graph_width = scaled_width
+        self.__graph_height = graph_height
+        self._window['-SCREEN-'].change_coordinates(
+            graph_bottom_left=(0, 0),
+            graph_top_right=(scaled_width, graph_height)
+        )
+        
+        self.__update_pygame_window()
+        return True
+
     def _before_exit(self, values = None) -> None:
         self.__stop.set()
 
@@ -154,20 +188,59 @@ class EmulatorWindow(BaseView):
             return
         self.__console.load_state(archive_path)
 
-    def __resize(self, original_image: np.ndarray) -> np.ndarray:
-        with self.__lock:
-            for event in pygame.event.get():
-                if event.type == pygame.VIDEORESIZE:
-                    self.cur_w, self.cur_h = event.w, event.h
-                    self.__game_screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-            resized_image = cv2.resize(original_image, (self.cur_h, self.cur_w))
-        return resized_image   
+    def __update_pygame_window(self):
+        # get -SCREEN- current resolution
+        try:
+            graph_width = self._window['-SCREEN-'].TKCanvas.winfo_width()
+            graph_height = self._window['-SCREEN-'].TKCanvas.winfo_height()
+        except Exception as e:
+            return
+        if graph_width <= 0 or graph_height <= 0:
+            return
+        
+        # calculate the position
+        x = (graph_width - self.__graph_width) / 2
+        y = 0
+        
+        # update pygame window position and size
+        if self.__system == 'Windows':
+            pygame_window = pygame.display.get_wm_info()['window']
+            ctypes.windll.user32.MoveWindow(
+                pygame_window, 
+                int(x), 
+                int(y), 
+                self.__graph_width, 
+                self.__graph_height, 
+                True
+            )
     
     def __run_file(self) -> None:
-        while not self.__stop.is_set():              
-            pressed = pygame.key.get_pressed()
+        self.__on_graph_resize(None)
+
+        pygame_window = None
+        if self.__system == 'Windows':
+            try:
+                pygame_window = pygame.display.get_wm_info()['window']
+            except Exception:
+                pygame_window = None
+
+        # 启动时切换一次焦点
+        if self.__system == 'Windows' and pygame_window:
+            ctypes.windll.user32.SetForegroundWindow(pygame_window)
+
+        while not self.__stop.is_set():
+            self._window.TKroot.update()
+            gui_event, gui_values = self._window.read(timeout=0)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.__stop.set()
+
+            # 不再每帧切换焦点
+            # ...其余代码不变...
+
             with open(KeyboardSettingWindow.keyboard_setting_path) as keyboard_setting:
                 keyboard = json.load(keyboard_setting)
+            pressed = pygame.key.get_pressed()
             self.__console.control([
                 pressed[keyboard['SELECT']],
                 pressed[keyboard['START']],
@@ -180,10 +253,14 @@ class EmulatorWindow(BaseView):
             ]) 
             self.__console.run()
             original_image = np.swapaxes(self.__console.bus.ppu.screen(), 0, 1)
-            resized_image = self.__resize(original_image)
-            surf = pygame.surfarray.make_surface(resized_image)
-            self.__game_screen.blit(surf, (0, 0))
+            surf = pygame.surfarray.make_surface(original_image)
+
+            target_size = self.__game_screen.get_size()
+            scaled_surf = pygame.transform.smoothscale(surf, target_size)
+            self.__game_screen.blit(scaled_surf, (0, 0))
+
             pygame.display.flip()
+            self.__update_pygame_window()
 
     def __run(self, values) -> None:
         success = self.__open_file()
